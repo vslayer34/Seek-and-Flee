a6ceb3d569bfe08052d77ed11046deae4e59af20
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-04-08 09:36:44 +0200

    3 - Evade

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class Bot : MonoBehaviour
 {
@@ -22,7 +23,8 @@ public class Bot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Pursue(cop);
+        //Pursue(cop);
+        Evade(cop);
     }
 
     void Seek(Vector3 target)
@@ -57,5 +59,30 @@ public class Bot : MonoBehaviour
             return;
         }
         Seek(target.position + target.transform.forward * lookAhead);
+    }
+
+
+    void Evade(Transform target)
+    {
+        Vector3 targetDirection = target.position - transform.position;
+        float lookAhead = targetDirection.magnitude / (agent.speed + dataTracker.copSpeed);
+
+        // calculate the angle between the two forward vectors of the robber and the cop
+        // if the angle is so small it should flee the position of the cop
+        float relativeHeading = Vector3.Angle(transform.forward, transform.TransformVector(target.transform.forward));
+
+        // angle between the forward direction of the agent and the angle to the taget
+        // if the angle greater than 90 the agent is in front of the cop and should flee his position
+        // if the angle is less than 90 the agent is behind the cop
+        float angleToTarget = Vector3.Angle(transform.forward, transform.TransformVector(targetDirection));
+
+
+        // flee the cop of he stops
+        if ((angleToTarget > 90.0f && relativeHeading < 20.0f) || dataTracker.copSpeed < 0.01f)
+        {
+            Flee(target.position);
+            return;
+        }
+        Flee(target.position + target.transform.forward * lookAhead);
     }
 }
